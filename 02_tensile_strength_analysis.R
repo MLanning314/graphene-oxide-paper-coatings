bfd31fd2e31fe4de102d688b2753bc6df5e0b39a
MLanning314 <mackenzielanning2026@u.northwestern.edu>
2026-03-06 12:39:01 -0500

    working on anova for machine direction and cross direction

--- 02_tensile_strength_analysis.R
+++ 02_tensile_strength_analysis.R
@@ -1,5 +1,13 @@
 
-# load packages 
+# Analysis of Data for Tensile Strength
+
+# Load Packages 
+library(tidyverse)
+library(here)
+
+# Load data
+load(here("data/tensile_strength_md.rda"))
+load(here("data/tensile_strength_cd.rda"))
 
 # machine direction anova
 md_anova <- aov(tensile_strength_n ~ coating, data = tensile_strength_md)
