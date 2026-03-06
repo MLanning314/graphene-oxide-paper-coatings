c871562976a22768393aa8a54f39cc20fe99a2b1
MLanning314 <mackenzielanning2026@u.northwestern.edu>
2026-03-06 12:38:07 -0500

    creating clean datasets

--- 01_data_cleaning.R
+++ 01_data_cleaning.R
@@ -4,4 +4,32 @@
 # Load Packages
 library(tidyverse)
 library(readxl)
+library(here)
+
+# Read in data from Excel
+tensile_data <- read_excel("Tensile_Strength_WI2026.xlsx",
+                                      sheet = "New Data") |>
+  janitor::clean_names() |>
+  mutate(
+    coating = as.factor(coating),
+    paper_type = as.factor(type_of_paper),
+    direction = as.factor(direction)
+    ) |>
+  select(-type_of_paper,
+         -date,
+         -average,
+         -standard_deviation)
+
+# make separate datasets for machine direction and cross direction
+tensile_strength_md <- tensile_data |>
+  filter(direction == "Machine Direction")
+
+tensile_strength_cd <- tensile_data |>
+  filter(direction == "Cross-Machine Direction")
+
+# save data
+save(tensile_data, file = here("data/tensile_data.rda"))
+save(tensile_strength_md, file = here("data/tensile_strength_md.rda"))
+save(tensile_strength_cd, file = here("data/tensile_strength_cd.rda"))
+
 
