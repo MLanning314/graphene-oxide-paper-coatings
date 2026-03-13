7d093e3a0e859f53e15cd4ede7e1b0a935282f5e
MLanning314 <mackenzielanning2026@u.northwestern.edu>
2026-03-13 18:03:53 -0400

    cleaned water absorption data

--- 01_data_cleaning.R
+++ 01_data_cleaning.R
@@ -35,4 +35,9 @@ save(tensile_strength_cd, file = here("data/tensile_strength_cd.rda"))
 
 # clean water absorption data 
 
+water_data_HF <- read_excel("HelloFresh_Absorption_Fall2025.xlsx",
+                            sheet = "Formatting for R") |>
+  janitor::clean_names() 
 
+# save data
+save(water_data_HF, file = here("data/water_data_HF.rda"))
