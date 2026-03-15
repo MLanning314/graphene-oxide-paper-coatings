797d7c27c8c868769e478c8230372a58b8525747
MLanning314 <mackenzielanning2026@u.northwestern.edu>
2026-03-15 17:54:46 -0400

    figure 4.1.3

--- 03_water_absorption_analysis.R
+++ 03_water_absorption_analysis.R
@@ -184,6 +184,50 @@ water_data_HF |>
   )
 
 # figure 4.1.13
+
+hf_3_fig <- water_data_HF_fig |>
+  filter(coating %in% c("0.2% GO", "0.5% GO", "15% WBBC")) |>
+  mutate(time = as.numeric(as.character(time))) 
+
+hf_3_img <- ggplot(hf_3_fig, aes(x = time, y = absorption,
+                                 color = coating, shape = coating)) +
+  geom_line(linewidth = 1) +
+  geom_point(size = 4) +
+  geom_errorbar(aes(ymin = absorption - rms,
+                    ymax = absorption + rms),
+                width = 1, linewidth = 0.5) +
+  scale_color_manual(
+    values = c("0.2% GO" = "dodgerblue",
+               "0.5% GO" = "firebrick",
+               "15% WBBC" = "goldenrod1")
+  ) +
+  scale_shape_manual(
+    values = c("0.2% GO" = 16,
+               "0.5% GO" = 17,
+               "15% WBBC" = 15)
+  ) +
+  scale_x_continuous(breaks = seq(0, 60, 10)) +
+  labs(
+    title = "0.2 wt%, 0.5 wt%, and 15% WBBC water absorption of HelloFresh",
+    x = "Time (min)",
+    y = "Absorbed water weight / unit dry paper weight (gm/gm)",
+    color = NULL,
+    shape = NULL
+  ) +
+  theme_classic(base_size = 12) +
+  theme(
+    plot.title = element_text(hjust = 0.5, size = 16),
+    axis.title.x = element_text(face = "plain"),
+    legend.position.inside = c(0.75, 0.25),
+    legend.background = element_blank(),
+    legend.text = element_text(size = 12)
+  )
+
+ggsave("figures/hf_3_img.png", plot = hf_3_img,
+       width = 10, height = 6, units = "in", dpi = 600)
+
+# anova
+
 hf_3 <- water_data_HF |>
   filter(coating %in% c("0.20% GO", "0.5% GO", "15% WBBC")) |>
   mutate(time = as.factor(time)) 
