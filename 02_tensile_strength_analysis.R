b5649d77dc8143caa1cf90ea2239c28d88d14bdd
MLanning314 <mackenzielanning2026@u.northwestern.edu>
2026-03-16 13:30:19 -0400

    working on figure 3.2.1

--- 02_tensile_strength_analysis.R
+++ 02_tensile_strength_analysis.R
@@ -21,6 +21,38 @@ summary(cd_anova)
 
 TukeyHSD(cd_anova)
 
-# seeing if groups are different from each other
-# group of DI and 0.1
-DI_0.1_anova <- aov()
+# figure 3.2.1
+# compute mean and sd per coating
+summary_3.2.1 <- tensile_strength_md |>
+  filter(coating %in% c("DI Water", "Stock", "0.1 wt% GO", "0.2 wt% GO",
+                        "0.35 wt% GO", "0.5 wt% GO")) |>
+  group_by(coating) |>
+  mutate(coating = factor(coating, 
+                          levels = c("DI Water", "Stock", 
+                                     "0.1 wt% GO", "0.2 wt% GO", 
+                                     "0.35 wt% GO", "0.5 wt% GO"))) |>
+  summarise(
+    mean_strength = mean(tensile_strength_n),
+    sd_strength = sd(tensile_strength_n),
+    .groups = "drop")
+
+# create figure 3.2.1
+fig_3.2.1 <- ggplot(summary_3.2.1, aes(x = coating, y = mean_strength, fill = coating)) +
+  geom_bar(stat = "identity", width = 0.7) +
+  geom_errorbar(aes(ymin = mean_strength - sd_strength, ymax = mean_strength + sd_strength),
+                width = 0.2, size = 0.5) +
+  scale_fill_manual(values = c(
+    "DI Water" = "dodgerblue",
+    "Stock" = "grey65",
+    "0.1 wt% GO" = "seagreen2",
+    "0.2 wt% GO" = "seagreen2",
+    "0.35 wt% GO" = "seagreen2",
+    "0.5 wt% GO" = "seagreen2"
+  )) +
+  scale_y_continuous(expand = expansion(mult = c(0, 0.05)), limits = c(0, 75)) +
+  labs(x = "", y = "Tensile Strength (N)", title = "Machine direction tensile strength of GO-coated HelloFresh sheets") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.position = "none",
+        plot.title = element_text(hjust = 0.5),
+        panel.grid.minor = element_blank())
