46a446198596040afded586fb9f5bee8aa019c18
MLanning314 <mackenzielanning2026@u.northwestern.edu>
2026-04-14 10:29:49 -0500

    cleaning figure 3.1.3

--- 03a_water_absorption_hellofresh.R
+++ 03a_water_absorption_hellofresh.R
@@ -123,7 +123,6 @@ fig_3.1.2 <- ggplot(hf_2_fig, aes(x = time, y = absorption,
   ) +
   theme_classic(base_size = 12) +
   theme(
-    plot.title = element_text(hjust = 0.5, size = 16),
     axis.title.x = element_text(face = "plain"),
     legend.position.inside = c(0.75, 0.20),
     legend.background = element_blank(),
@@ -135,91 +134,58 @@ ggsave("figures/fig_3.1.2.png", plot = fig_3.1.2,
 
 # anova for DI water, 0.1% GO, 0.2% GO, 0.5% GO, and stock
 
-hf_2 <- water_data_HF |>
+hf_2 <- absorption_data_HF |>
   filter(coating %in% c("DI Water", "Stock", "0.1% GO", "0.20% GO", "0.5% GO")) |>
   mutate(time = as.factor(time)) 
 
 hf_2_60 <- hf_2 |>
   filter(time == 60)
 
-fit_oneway_3.1.2 <- aov(raw_weight ~ coating, data = hf_2)
+fit_oneway_3.1.2 <- aov(absorption ~ coating, data = hf_2)
 summary(fit_oneway_3.1.2)
 
-fit_oneway_3.1.2_60 <- aov(raw_weight ~ coating, data = hf_2_60)
+fit_oneway_3.1.2_60 <- aov(absorption ~ coating, data = hf_2_60)
 summary(fit_oneway_3.1.2_60)
 
 # If you want to account for time as well (recommended)
-fit_twoway_3.1.2 <- aov(raw_weight ~ coating * time, data = hf_2)
+fit_twoway_3.1.2 <- aov(absorption ~ coating * time, data = hf_2)
 summary(fit_twoway_3.1.2)
 
 TukeyHSD(fit_oneway_3.1.2, "coating")
 TukeyHSD(fit_oneway_3.1.2_60, "coating")
 
 # see differences in variance of coatings
-leveneTest(raw_weight ~ coating, data = hf_2)
-
-# visualize coating variability
-fig_3.1.2_variability <- ggplot(hf_2, aes(x = coating, y = raw_weight, fill = coating)) +
-  geom_boxplot() +
-  scale_fill_manual(values = c(
-    "DI Water" = "dodgerblue",   
-    "Stock" = "grey65",      
-    "0.1% GO" = "firebrick",   
-    "0.20% GO" = "goldenrod1",    
-    "0.5% GO" = "springgreen1"     
-  )) +
-  ylab("Water Absorption") +
-  xlab("Coating") +
-  theme_minimal() +
-  theme(legend.position = "none")  
-
-hf_2 |>
-  filter(time == 60) |>
-  group_by(coating) |>
-  summarize(
-    sd_absorption = sd(raw_weight)
-  )
+leveneTest(absorption ~ coating, data = hf_2)
 
 
-# get mean and sd at 60 minutes to see absorption differences
-water_data_HF |> 
-  filter(coating %in% c("DI Water", "Stock", "0.1% GO", "0.20% GO", 
-                          "0.5% GO"),
-         time == "60") |> 
-  group_by(coating) |> 
-  summarise(
-    mean_abs = round(mean(raw_weight, na.rm = TRUE), 3),
-    sd_abs   = round(sd(raw_weight, na.rm = TRUE), 3),
-    n        = n(),  # sample size per group
-    .groups = "drop"
-  )
+# formatting for figure 3.1.3
 
-# figure 3.1.3
-
-hf_3_fig <- water_data_HF_fig |>
+hf_3_fig <- absorption_HF_fig |>
   filter(coating %in% c("0.2% GO", "0.5% GO", "15% WBBC")) |>
-  mutate(time = as.numeric(as.character(time))) 
+  mutate(time = as.numeric(time),
+         coating = factor(
+           dplyr::recode(coating, `15% WBBC` = "5.85% Joncryl"),
+           levels = c("0.2% GO", "0.5% GO", "5.85% Joncryl")))
 
-hf_3_img <- ggplot(hf_3_fig, aes(x = time, y = absorption,
+fig_3.1.3 <- ggplot(hf_3_fig, aes(x = time, y = absorption,
                                  color = coating, shape = coating)) +
   geom_line(linewidth = 1) +
   geom_point(size = 4) +
   geom_errorbar(aes(ymin = absorption - rms,
                     ymax = absorption + rms),
-                width = 1, linewidth = 0.5) +
+                width = 0.1, linewidth = 0.5) +
   scale_color_manual(
     values = c("0.2% GO" = "dodgerblue",
                "0.5% GO" = "firebrick",
-               "15% WBBC" = "goldenrod1")
+               "5.85% Joncryl" = "goldenrod1")
   ) +
   scale_shape_manual(
     values = c("0.2% GO" = 16,
                "0.5% GO" = 17,
-               "15% WBBC" = 15)
+               "5.85% Joncryl" = 15)
   ) +
   scale_x_continuous(breaks = seq(0, 60, 10)) +
   labs(
-    title = "0.2 wt%, 0.5 wt%, and 15% WBBC water absorption of HelloFresh",
     x = "Time (min)",
     y = "Absorbed water weight / unit dry paper weight (gm/gm)",
     color = NULL,
@@ -227,14 +193,13 @@ hf_3_img <- ggplot(hf_3_fig, aes(x = time, y = absorption,
   ) +
   theme_classic(base_size = 12) +
   theme(
-    plot.title = element_text(hjust = 0.5, size = 16),
     axis.title.x = element_text(face = "plain"),
     legend.position.inside = c(0.75, 0.25),
     legend.background = element_blank(),
     legend.text = element_text(size = 12)
   )
 
-ggsave("figures/hf_3_img.png", plot = hf_3_img,
+ggsave("figures/fig_3.1.3.png", plot = fig_3.1.3,
        width = 10, height = 6, units = "in", dpi = 600)
 
 # anova
