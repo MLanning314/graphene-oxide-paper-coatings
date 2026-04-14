6c43c29cde0653c874b99df8beca37dc5fbe74c5
MLanning314 <mackenzielanning2026@u.northwestern.edu>
2026-04-14 14:53:59 -0500

    working on figure 3.3.1

--- 02_tensile_strength_analysis.R
+++ 02_tensile_strength_analysis.R
@@ -15,9 +15,9 @@ summary(cd_anova)
 
 TukeyHSD(cd_anova)
 
-# figure 3.2.1
+# figure 3.3.1
 # compute mean and sd per coating
-summary_3.2.1 <- tensile_strength_md |>
+md_3.3.1 <- tensile_strength_md |>
   group_by(coating) |>
   mutate(coating = factor(coating, 
                           levels = c("DI Water", "Stock", 
@@ -32,7 +32,7 @@ summary_3.2.1 <- tensile_strength_md |>
     .groups = "drop")
 
 # create figure 3.2.1
-fig_3.2.1 <- ggplot(summary_3.2.1, aes(x = coating, y = mean_strength, fill = coating)) +
+fig_3.3.1 <- ggplot(md_3.3.1, aes(x = coating, y = mean_strength, fill = coating)) +
   geom_bar(stat = "identity", width = 0.7) +
   geom_errorbar(aes(ymin = mean_strength - sd_strength, ymax = mean_strength + sd_strength),
                 width = 0.2, size = 0.5) +
@@ -49,15 +49,14 @@ fig_3.2.1 <- ggplot(summary_3.2.1, aes(x = coating, y = mean_strength, fill = co
     "15% WBBC + 0.1% GO" = "seagreen2"
   )) +
   scale_y_continuous(expand = expansion(mult = c(0, 0.05)), limits = c(0, 75)) +
-  labs(x = "", y = "Tensile Strength (N)", title = "Machine direction tensile strength of GO-coated HelloFresh sheets") +
+  labs(x = "", y = "Tensile Strength (N)") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),
         legend.position = "none",
-        plot.title = element_text(hjust = 0.5),
         panel.grid.major = element_blank(),   
         panel.grid.minor = element_blank())
 
-ggsave("figures/fig_3.2.1.png", plot = fig_3.2.1,
+ggsave("figures/fig_3.3.1.png", plot = fig_3.3.1,
        width = 10, height = 6, units = "in", dpi = 600)
 
 # machine direction anova
