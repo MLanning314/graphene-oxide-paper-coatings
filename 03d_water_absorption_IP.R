82725224566fa79108844ebb59d1c24a2e873c8e
MLanning314 <mackenzielanning2026@u.northwestern.edu>
2026-04-15 15:11:43 -0500

    working on IP absorption figures and statistics

--- 03d_water_absorption_IP.R
+++ 03d_water_absorption_IP.R
@@ -57,3 +57,95 @@ fig_4.3.1 <- ggplot(IP_1_fig, aes(x = time, y = absorption,
 
 ggsave("figures/fig_4.3.1.png", plot = fig_4.3.1,
        width = 10, height = 6, units = "in", dpi = 600)
+
+# anova for DI water, Stock, 0.1% GO, and 25% WBBC
+IP_1 <- absorption_data_IP |>
+  filter(coating %in% c("DI Water", "0.1% GO", "Stock", "25% WBBC")) |>
+  mutate(time = as.factor(time)) 
+
+IP_1_60 <- IP_1 |>
+  filter(time == 60)
+
+fit_oneway_4.3.1 <- aov(absorption ~ coating, data = IP_1)
+summary(fit_oneway_4.3.1)
+
+fit_oneway_4.3.1_60 <- aov(absorption ~ coating, data = IP_1_60)
+summary(fit_oneway_4.3.1_60)
+
+fit_twoway_4.3.1 <- aov(absorption ~ coating * time, data = IP_1)
+summary(fit_twoway_4.3.1)
+
+TukeyHSD(fit_oneway_4.3.1)
+TukeyHSD(fit_twoway_4.3.1)
+TukeyHSD(fit_oneway_4.3.1_60)
+
+# formatting for figure 4.3.2
+IP_1_fig <- absorption_IP_fig |>
+  filter(coating %in% c("DI Water", "0.1% GO", "Stock",
+                        "25% WBBC")) |>
+  mutate(time = as.numeric(time),
+         coating = dplyr::recode(coating,
+                                 `25% WBBC` = "9.75% Joncryl"),
+         coating = factor(coating,
+                          levels = c("Stock", "DI Water", "0.1% GO", "9.75% Joncryl")))
+
+fig_4.3.1 <- ggplot(IP_1_fig, aes(x = time, y = absorption,
+                                  color = coating, shape = coating)) +
+  geom_line(linewidth = 1) +
+  geom_point(size = 4) +
+  geom_errorbar(aes(ymin = absorption - rms,
+                    ymax = absorption + rms),
+                width = 0.1, linewidth = 0.5) +
+  scale_color_manual(
+    values = c("DI Water" = "dodgerblue",
+               "Stock" = "grey65",
+               "0.1% GO" = "firebrick",
+               "9.75% Joncryl" = "goldenrod1"
+    )
+  ) +
+  scale_shape_manual(
+    values = c("DI Water" = 16,
+               "0.1% GO" = 17,
+               "Stock" = 15,
+               "9.75% Joncryl" = 18)
+  ) +
+  scale_x_continuous(breaks = seq(0, 60, 10)) +
+  labs(
+    x = "Time (min)",
+    y = "Absorbed water weight / unit dry paper weight (gm/gm)",
+    color = NULL,
+    shape = NULL
+  ) +
+  theme_classic(base_size = 12) +
+  theme(
+    axis.title.x = element_text(face = "plain"),
+    legend.position.inside = c(0.75, 0.25),
+    legend.background = element_blank(),
+    legend.text = element_text(size = 12)
+  )
+
+ggsave("figures/fig_4.3.1.png", plot = fig_4.3.1,
+       width = 10, height = 6, units = "in", dpi = 600)
+
+# anova for DI water, Stock, 0.1% GO, and 25% WBBC
+IP_1 <- absorption_data_IP |>
+  filter(coating %in% c("DI Water", "0.1% GO", "Stock", "25% WBBC")) |>
+  mutate(time = as.factor(time)) 
+
+IP_1_60 <- IP_1 |>
+  filter(time == 60)
+
+fit_oneway_4.3.1 <- aov(absorption ~ coating, data = IP_1)
+summary(fit_oneway_4.3.1)
+
+fit_oneway_4.3.1_60 <- aov(absorption ~ coating, data = IP_1_60)
+summary(fit_oneway_4.3.1_60)
+
+fit_twoway_4.3.1 <- aov(absorption ~ coating * time, data = IP_1)
+summary(fit_twoway_4.3.1)
+
+TukeyHSD(fit_oneway_4.3.1)
+TukeyHSD(fit_twoway_4.3.1)
+TukeyHSD(fit_oneway_4.3.1_60)
+
+
