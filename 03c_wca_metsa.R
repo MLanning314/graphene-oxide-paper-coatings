6a90669bfc78d6e4a586b22e1f427d8d1eb6bfe1
MLanning314 <mackenzielanning2026@u.northwestern.edu>
2026-04-15 20:11:59 -0500

    adding sessile volume to wca data

--- 03c_wca_metsa.R
+++ 03c_wca_metsa.R
@@ -14,8 +14,53 @@ load(here("data/wca_data_metsa.rda"))
 wca_1_fig <- wca_data_metsa |>
   filter(coating %in% c("DI Water", "0.1% GO", "Stock"))
 
-wca_1_img <- ggplot(wca_1_fig, aes(x = time, y = contact_angle,
+fig_4.2.1 <- ggplot(wca_1_fig, aes(x = time, y = contact_angle,
                                        color = coating, shape = coating)) +
+  geom_line(linewidth = 1) +
+  geom_point(size = 2) +
+  scale_color_manual(
+    values = c("DI Water" = "dodgerblue",
+               "Stock" = "grey65",
+               "0.1% GO" = "firebrick")
+  ) +
+  scale_shape_manual(
+    values = c("DI Water" = 16,
+               "0.1% GO" = 17,
+               "Stock" = 15)
+  ) +
+  scale_x_continuous(breaks = seq(0, 600, 100)) +
+  labs(
+    x = "Time (s)",
+    y = "Absorbed water weight / unit dry paper weight (gm/gm)",
+    color = NULL,
+    shape = NULL
+  ) +
+  theme_classic(base_size = 12) +
+  theme(
+    axis.title.x = element_text(face = "plain"),
+    legend.position.inside = c(0.75, 0.25),
+    legend.background = element_blank(),
+    legend.text = element_text(size = 12)
+  )
+
+ggsave("figures/fig_4.2.1.png", plot = fig_4.2.1,
+       width = 10, height = 6, units = "in", dpi = 600)
+
+# statistics for DI water, Stock, 0.1% GO
+
+wca_lm_1 <- lm(contact_angle ~ coating * time, data = wca_1_fig)
+anova(wca_lm_1)
+summary(wca_lm_1)
+
+# time-specific estimated means and pairwise differences
+wca_1_emm <- emmeans(wca_lm_1, ~ coating | time,
+                 at = list(time = c(100, 600)))
+wca_1_emm                 
+pairs(wca_1_emm)   
+
+# formatting for figure 4.2.2
+fig_4.2.2 <- ggplot(wca_1_fig, aes(x = time, y = contact_angle,
+                                   color = coating, shape = coating)) +
   geom_line(linewidth = 1) +
   geom_point(size = 2) +
   scale_color_manual(
@@ -56,9 +101,10 @@ summary(wca_lm_1)
 
 # time-specific estimated means and pairwise differences
 wca_1_emm <- emmeans(wca_lm_1, ~ coating | time,
-                 at = list(time = c(100, 600)))
+                     at = list(time = c(100, 600)))
 wca_1_emm                 
 pairs(wca_1_emm)            
+
 
 # formatting for figure 4.1.4
 wca_2_fig <- wca_data_metsa |>
