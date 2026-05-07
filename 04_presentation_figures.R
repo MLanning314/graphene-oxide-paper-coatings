fc96ddb6b8178995b890ca299b6c5074b01950f1
MLanning314 <mackenzielanning2026@u.northwestern.edu>
2026-05-07 14:53:35 -0500

    tensile strength figure for presentation

--- 04_presentation_figures.R
+++ 04_presentation_figures.R
@@ -9,6 +9,7 @@ library(car)
 # Load Data
 load(here("data/absorption_data_HF.rda"))
 load(here("data/absorption_HF_fig.rda"))
+load(here("data/tensile_strength_md.rda"))
 
 # Figure 1: DI Water, 0.1% GO, 9.75% Joncryl - HelloFresh
 
@@ -113,3 +114,41 @@ pres_fig_2 <- ggplot(pres_2_fig, aes(x = time, y = absorption,
 
 ggsave("presentation_figures/pres_fig_2.png", plot = pres_fig_2,
        width = 10, height = 6, units = "in", dpi = 600)
+
+# Figure 3 - Tensile Strength HelloFresh
+
+md_pres <- tensile_strength_md |>
+  group_by(coating) |>
+  mutate(coating = factor(coating, 
+                          levels = c("DI Water", "Stock", 
+                                     "0.1 wt% GO",
+                                     "15% WBBC",
+                                     "15% WBBC + 0.1% GO"))) |>
+  summarise(
+    mean_strength = mean(tensile_strength_n),
+    sd_strength = sd(tensile_strength_n),
+    .groups = "drop")
+
+# create figure 3.3.1
+pres_fig_3 <- ggplot(md_pres, aes(x = coating, y = mean_strength, fill = coating)) +
+  geom_bar(stat = "identity", width = 0.7) +
+  geom_errorbar(aes(ymin = mean_strength - sd_strength, ymax = mean_strength + sd_strength),
+                width = 0.2, size = 0.5) +
+  scale_fill_manual(values = c(
+    "DI Water" = "dodgerblue",
+    "Stock" = "grey65",
+    "0.1 wt% GO" = "firebrick",
+    "15% WBBC" = "goldenrod1",
+    "15% WBBC + 0.1% GO" = "seagreen2"
+  )) +
+  scale_y_continuous(expand = expansion(mult = c(0, 0.05)), limits = c(0, 75)) +
+  labs(x = "", y = "Tensile Strength (N)") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, color = "black"),
+        legend.position = "none",
+        panel.grid.major = element_blank(),   
+        panel.grid.minor = element_blank())
+
+ggsave("presentation_figures/pres_fig_3.png", plot = pres_fig_3,
+       width = 10, height = 6, units = "in", dpi = 600)
+
