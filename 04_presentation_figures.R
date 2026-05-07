1facd9d4914bc8554120cd09efd6b2c1d74792cb
MLanning314 <mackenzielanning2026@u.northwestern.edu>
2026-05-07 17:27:51 -0500

    working on figure 4 for presentation

--- 04_presentation_figures.R
+++ 04_presentation_figures.R
@@ -122,8 +122,9 @@ md_pres <- tensile_strength_md |>
   filter(coating %in% c("DI Water", "Stock", "0.1 wt% GO",
                         "15% WBBC", "15% WBBC + 0.1% GO")) |>
   mutate(coating = factor(coating, 
-                          levels = c("DI Water", "Stock", 
+                          levels = c("DI Water",
                                      "0.1 wt% GO",
+                                     "Stock", 
                                      "15% WBBC",
                                      "15% WBBC + 0.1% GO"),
                           labels = c("DI Water", "Stock",
@@ -135,7 +136,6 @@ md_pres <- tensile_strength_md |>
     sd_strength = sd(tensile_strength_n),
     .groups = "drop")
 
-# create figure 3.3.1
 pres_fig_3 <- ggplot(md_pres, aes(x = coating, y = mean_strength, fill = coating)) +
   geom_bar(stat = "identity", width = 0.7) +
   geom_errorbar(aes(ymin = mean_strength - sd_strength, ymax = mean_strength + sd_strength),
@@ -162,4 +162,64 @@ pres_fig_3 <- ggplot(md_pres, aes(x = coating, y = mean_strength, fill = coating
 
 ggsave("presentation_figures/pres_fig_3.png", plot = pres_fig_3,
        width = 10, height = 6, units = "in", dpi = 600)
+
+# Figure 4 - Basically Figure 2 but with Stock included
+pres_4_fig <- absorption_HF_fig |>
+  filter(coating %in% c("DI Water", "Stock", "25% WBBC", "25% WBBC + 0.1% GO", 
+                        "0.1% GO")) |>
+  mutate(
+    time = (as.numeric(time) - 1) * 10,
+    coating = dplyr::recode(coating,
+                            `25% WBBC + 0.1% GO` = "9.75% Joncryl + 0.1% GO",
+                            `25% WBBC` = "9.75% Joncryl"),
+    coating = factor(coating,
+                     levels = c("DI Water",
+                                "Stock",
+                                "0.1% GO",
+                                "9.75% Joncryl",
+                                "9.75% Joncryl + 0.1% GO")))
+
+
+pres_fig_4 <- ggplot(pres_4_fig, aes(x = time, y = absorption,
+                                     color = coating, shape = coating)) +
+  geom_line(linewidth = 1) +
+  geom_point(size = 4) +
+  geom_errorbar(aes(ymin = absorption - rms,
+                    ymax = absorption + rms),
+                width = 1, linewidth = 0.5) +
+  scale_color_manual(
+    values = c("DI Water" = "dodgerblue",
+               "Stock" = "grey65",
+               "0.1% GO" = "firebrick",
+               "9.75% Joncryl" = "goldenrod1",
+               "9.75% Joncryl + 0.1% GO" = "springgreen1"
+    )) +
+  scale_shape_manual(
+    values = c( "DI Water" = 16,
+                "Stock" = 20,
+                "0.1% GO" = 15,
+                "9.75% Joncryl" = 18,
+                "9.75% Joncryl + 0.1% GO" = 17)
+  ) +
+  scale_x_continuous(breaks = seq(10, 60, 10)) +
+  coord_cartesian(xlim = c(0, 60)) +
+  labs(
+    x = "Time (min)",
+    y = "Absorbed water weight / unit dry paper weight (gm/gm)",
+    color = NULL,
+    shape = NULL
+  ) +
+  theme_classic(base_size = 12) +
+  theme(
+    axis.title.x = element_text(size = 16, face = "plain"),
+    axis.title.y = element_text(size = 16, face = "plain"),
+    legend.position.inside = c(0.75, 0.25),
+    legend.background = element_blank(),
+    legend.text = element_text(size = 12)
+  )
+
+ggsave("presentation_figures/pres_fig_4.png", plot = pres_fig_4,
+       width = 10, height = 6, units = "in", dpi = 600)
+
+
 
