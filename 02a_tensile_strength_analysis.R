bd19c624469e5d85b85c819d8953cfd0195f74d1
MLanning314 <mackenzielanning2026@u.northwestern.edu>
2026-05-11 09:04:05 -0500

    tensile strength figures

--- 02a_tensile_strength_analysis.R
+++ 02a_tensile_strength_analysis.R
@@ -77,13 +77,20 @@ TukeyHSD(md_anova)
 cd_3.3.3 <- tensile_strength_cd |>
   filter(coating != "0.15 wt% GO") |>
   group_by(coating) |>
-  mutate(coating = factor(coating, 
-                          levels = c("DI Water", "Stock", 
-                                     "0.1 wt% GO", "0.2 wt% GO", 
-                                     "0.35 wt% GO", "0.5 wt% GO",
-                                     "5% WBBC", "15% WBBC",
-                                     "5% WBBC + 0.1% GO",
-                                     "15% WBBC + 0.1% GO"))) |>
+  mutate(coating = dplyr::recode(
+    coating,
+    `5% WBBC` = "1.95 wt% Joncryl",
+    `15% WBBC` = "5.85 wt% Joncryl",
+    `5% WBBC + 0.1% GO` = "1.95 wt% Joncryl + 0.1 wt% GO",
+    `15% WBBC + 0.1% GO` = "5.85 wt% Joncryl + 0.1 wt% GO"
+  ),
+  coating = factor(coating, 
+                   levels = c("DI Water", "Stock", 
+                              "0.1 wt% GO", "0.2 wt% GO", 
+                              "0.35 wt% GO", "0.5 wt% GO",
+                              "1.95 wt% Joncryl", "5.85 wt% Joncryl",
+                              "1.95 wt% Joncryl + 0.1 wt% GO",
+                              "5.85 wt% Joncryl + 0.1 wt% GO"))) |>
   summarise(
     mean_strength = mean(tensile_strength_n),
     sd_strength = sd(tensile_strength_n),
@@ -101,10 +108,10 @@ fig_3.3.3 <- ggplot(cd_3.3.3, aes(x = coating, y = mean_strength, fill = coating
     "0.2 wt% GO" = "firebrick",
     "0.35 wt% GO" = "firebrick",
     "0.5 wt% GO" = "firebrick",
-    "5% WBBC" = "goldenrod1",
-    "15% WBBC" = "goldenrod1",
-    "5% WBBC + 0.1% GO" = "seagreen2",
-    "15% WBBC + 0.1% GO" = "seagreen2"
+    "1.95 wt% Joncryl" = "goldenrod1",
+    "5.85 wt% Joncryl" = "goldenrod1",
+    "1.95 wt% Joncryl + 0.1 wt% GO" = "seagreen2",
+    "5.85 wt% Joncryl + 0.1 wt% GO" = "seagreen2"
   )) +
   scale_y_continuous(expand = expansion(mult = c(0, 0.05)), limits = c(0, 40)) +
   labs(x = "", y = "Tensile Strength (N)") +
