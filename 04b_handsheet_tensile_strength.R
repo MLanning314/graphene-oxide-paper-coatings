73e6d04b83309607115947b5174163ccbfe71732
MLanning314 <mackenzielanning2026@u.northwestern.edu>
2026-05-28 22:10:56 -0500

    working on tensile strength

--- 04b_handsheet_tensile_strength.R
+++ 04b_handsheet_tensile_strength.R
@@ -1,2 +1,22 @@
 
 # Tensile Strength Analysis for Handsheets
+
+# Load Packages 
+library(tidyverse)
+library(here)
+library(car)
+
+# Load data
+load(here("data/tensile_data_clammp.rda"))
+load(here("data/distance_data_clammp.rda"))
+
+# filter out handsheet data
+handsheet_strength <- tensile_data_clammp |>
+  filter(paper_type == "Greif")
+
+# tensile strength of handsheet samples
+handsheet_strength_aov <- aov(strength_n ~ coating, data = handsheet_strength)
+summary(handsheet_strength_aov)
+
+TukeyHSD(handsheet_strength_aov)
+leveneTest(strength_n ~ coating, data = handsheet_strength)
