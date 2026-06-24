2a9d615a45dac99eca0e56b3acf3405d699789ca
MLanning314 <mackenzielanning2026@u.northwestern.edu>
2026-06-24 15:22:05 -0500

    working on filtering out ASA and AKD data

--- 04b_handsheet_tensile_strength-Bellow2.R
+++ 04b_handsheet_tensile_strength-Bellow2.R
@@ -15,6 +15,14 @@ load(here("data/tensile_burst_index.rda"))
 handsheet_strength <- tensile_data_clammp |>
   filter(paper_type == "Greif")
 
+# AKD samples
+handsheet_strength_akd <- tensile_burst_index |>
+  filter(condition %in% c("H1", "H2", "H3"))
+
+# ASA samples
+handsheet_strength_asa <- tensile_burst_index |>
+  filter(condition %in% c("H4", "H5", "H6"))
+
 # tensile strength of handsheet samples
 handsheet_strength_aov <- aov(strength_n ~ coating, data = handsheet_strength)
 summary(handsheet_strength_aov)
@@ -26,6 +34,14 @@ leveneTest(strength_n ~ coating, data = handsheet_strength)
 tensile_index_j <- aov(tensile_index_j ~ condition, data = tensile_burst_index)
 summary(tensile_index_j)
 TukeyHSD(tensile_index_j)
+
+tensile_index_akd_j <- aov(tensile_index_j ~ condition, data = handsheet_strength_akd)
+summary(tensile_index_akd_j)
+TukeyHSD(tensile_index_akd_j)
+
+tensile_index_asa_j <- aov(tensile_index_j ~ condition, data = handsheet_strength_asa)
+summary(tensile_index_asa_j)
+TukeyHSD(tensile_index_asa_j)
 
 tensile_index_m <- aov(tensile_index_m ~ condition, data = tensile_burst_index)
 summary(tensile_index_m)
