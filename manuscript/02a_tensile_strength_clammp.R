77cf4617aab500f75326e24d8e65304604237757
MLanning314 <mackenzielanning2026@u.northwestern.edu>
2026-07-21 17:07:39 -0400

    cleaning up tensile strength script for manuscript

--- manuscript/02a_tensile_strength_clammp.R
+++ manuscript/02a_tensile_strength_clammp.R
@@ -1,28 +1,48 @@
 
 # Analysis of Data for Tensile Strength - CLaMMP ------------------------------------------------------
 
-# Load Packages 
+## Load Packages ----
 library(tidyverse)
 library(here)
 library(car)
 
-# Load data
+## Load Data ----
 load(here("manuscript/data/tensile_data_small.rda"))
 load(here("manuscript/data/tensile_data_large.rda"))
 
-# Filter out Cross-Machine Direction and Machine Direction
+## Filter out Cross-Machine Direction and Machine Direction ----
 large_anova_cd <- tensile_data_large |>
   filter(direction == "Cross-Machine")
 
-# Statistics for Tensile Strength ClaMMP
-large_anova <- aov(strength_n ~ coating, data = large_anova_cd)
-summary(large_anova)
+large_anova_md <- tensile_data_large |>
+  filter(direction == "Machine")
 
-TukeyHSD(large_anova)
-leveneTest(strength_n ~ coating, data = tappi_clammp_md)
+small_anova_cd <- tensile_data_small |>
+  filter(direction == "Cross-Machine")
 
-small_md_anova <- aov(strength_n ~ coating, data = small_clammp_md)
-summary(small_md_anova)
+small_anova_md <- tensile_data_small |>
+  filter(direction == "Machine")
 
-TukeyHSD(small_md_anova)
-leveneTest(strength_n ~ coating, data = small_clammp_md)
+## Statistics for Tensile Strength ClaMMP ----
+
+### Cross-Machine Direction Analysis ----
+aov_cd_large <- aov(strength_n ~ coating, data = large_anova_cd)
+summary(aov_cd_large)
+
+TukeyHSD(aov_cd_large)
+leveneTest(strength_n ~ coating, data = large_anova_cd)
+
+### Machine Direction Analysis ----
+# tappi standard samples
+aov_md_large <- aov(strength_n ~ coating, data = large_anova_md)
+summary(aov_md_large)
+
+TukeyHSD(aov_md_large)
+leveneTest(strength_n ~ coating, data = large_anova_md)
+
+# smaller sized samples
+aov_md_small <- aov(strength_n ~ coating, data = small_anova_md)
+summary(aov_md_small)
+
+TukeyHSD(aov_md_small)
+leveneTest(strength_n ~ coating, data = small_anova_md)
